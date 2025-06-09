d90b06fbb8dbd4cd1ed0ea3fdcf0bf3f96c7e718
Luis Fernando <luisfgz419@gmail.com>
2025-06-08 23:04:59 -0600

    Agregados archivos js de búsqueda generados por Doxygen

---
+++ Output/html/search/all_19.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['–_20airland_20word_20adventure_20versión_202_200_0',['Manual de Usuario – Airland Word Adventure (Versión 2.0)',['../C:/Users/User/AirlandWordsAdventure/Assets/Script/Scripts/README.md#autotoc_md0',1,'']]]
+];
